49fae9b98e3775cf2c4acee04846d76144e9465a
WaterFish <wf121506@gmail.com>
2023-11-29 22:30:45 +0800

    first commit

---
+++ clent/src/router/index.ts
@@ -0,0 +1,23 @@
+import { RouteRecordRaw, createRouter, createWebHistory } from 'vue-router'
+
+const routes: Array<RouteRecordRaw> = [
+	{
+		path: '/',
+		redirect: {
+			name: 'Home',
+		},
+	},
+	{
+		path: '/:catchAll(.*)',
+		redirect: {
+			name: 'Home',
+		},
+	},
+]
+
+const router = createRouter({
+	history: createWebHistory(import.meta.env.BASE_URL),
+	routes,
+})
+
+export default router
